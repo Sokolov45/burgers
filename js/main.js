2bc80fa556b9e023c1832b1af072f362f8d14c66
Anton Sokolov <zuk45@bk.ru>
2019-12-05 01:17:24 +0500

    сделал переключатель у слайдера

--- js/main.js
+++ js/main.js
@@ -1,36 +1,43 @@
-var slides = document.getElementsByClassName("burgers__item");
+var slides = document.getElementsByClassName("slide");
 var currentSlide = 0;
 
-function showSlides() {
-  slides[currentSlide].className = 'visible';
-  currentSlide = (currentSlide+1)%slides.length;
-  slides[currentSlide].className = 'slide showing';
-}
-
-
-
-  if (n > slides.length) {
-    n = 0;
-  }if else (n < 0) {
-    slideIndex = slides.length
-  }
-
-  // for (var i = 0; i < slides.length; i++) {
-  //   slides[i].style.display = "none";
-  // }
-
-  slides[currentSlide].className += "--active";
-}
-
-
-function nextSlide() {
-  showSlides(currentSlide += 1);
+// function showSlide(currentSlide) {
+//     if (currentSlide > slides.length) {
+//         currentSlide = 0;
+//       }if else (currentSlide < 0) {
+//           currentSlide = slides.length
+//         }
+//           slides[currentSlide].className = 'visible';
+//           for (var i=0; i<=slides.length; i++){
+//               slides[i].className = 'slide';
+//             }
+//         }
+        
+        function nextSlide() {
+          // showSlides(currentSlide += 1);
+          if (currentSlide > slides.length) {
+            currentSlide == 0;
+          }
+// alert(slides.length);
+          
+          slides[currentSlide].className = 'slide';
+          ++currentSlide;
+          slides[currentSlide].className = 'slide visible';
+          if (currentSlide == 4) {
+            slides[currentSlide].className = 'slide';
+            currentSlide = 0;
+            slides[currentSlide].className = 'slide visible'
+                       
+          }
+// alert(slides
+          return currentSlide;
+  
 }
 
 /* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-function previosSlide() {
-  showSlides(currentSlide -= 1);  
-}
+// function previosSlide() {
+//   showSlides(currentSlide -= 1);  
+// }
 
 
-showSlides(currentSlide);
+// element.removeEventListener(event, handler[, options]);
