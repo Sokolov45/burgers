0ebc42ee96691f19b4b517692aa2cc0e78274de2
Anton Sokolov <zvuk45@bk.ru>
2019-12-05 18:14:26 +0500

    сделан слайдер у бургеров

--- js/main.js
+++ js/main.js
@@ -1,43 +1,70 @@
-var slides = document.getElementsByClassName("slide");
-var currentSlide = 0;
+// var slides = document.getElementsByClassName("slide");
+// var currentSlide = 0;
 
-// function showSlide(currentSlide) {
-//     if (currentSlide > slides.length) {
-//         currentSlide = 0;
-//       }if else (currentSlide < 0) {
-//           currentSlide = slides.length
-//         }
-//           slides[currentSlide].className = 'visible';
-//           for (var i=0; i<=slides.length; i++){
-//               slides[i].className = 'slide';
-//             }
-//         }
-        
-        function nextSlide() {
-          // showSlides(currentSlide += 1);
-          if (currentSlide > slides.length) {
-            currentSlide == 0;
-          }
-// alert(slides.length);
-          
-          slides[currentSlide].className = 'slide';
-          ++currentSlide;
-          slides[currentSlide].className = 'slide visible';
-          if (currentSlide == 4) {
-            slides[currentSlide].className = 'slide';
-            currentSlide = 0;
-            slides[currentSlide].className = 'slide visible'
-                       
-          }
-// alert(slides
-          return currentSlide;
-  
-}
+// function nextSlide() {
+//   for (var i=0; i < slides.length; i++){
+//     slides[i].className = 'slide';
+//   }
 
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-// function previosSlide() {
-//   showSlides(currentSlide -= 1);  
+//   slides[currentSlide].className = 'slide visible';
+
+//   if (currentSlide == slides.length-1) {
+//     currentSlide = 0;
+//     return currentSlide;
+//   }
+
+//   return currentSlide++;
+// }
+
+// function prevSlide() {
+//   for (var i=0; i < slides.length; i++){
+//     slides[i].className = 'slide';
+//   }
+//   slides[currentSlide].className = 'slide visible';
+//   if (currentSlide < 0) {
+//     currentSlide = slides.length - 1;
+//     return currentSlide;
+//   }
+//   return currentSlide = currentSlide - 1;
 // }
 
 
-// element.removeEventListener(event, handler[, options]);
+
+
+// nextSlide();
+
+
+
+var slides = document.getElementsByClassName("slide");
+var currentSlide = 0;
+
+function show(currentSlide) {
+  for (var i=0; i < slides.length; i++){
+    slides[i].className = 'slide';
+  }
+  slides[currentSlide].className = 'slide visible';
+}
+
+function nextSlide() {
+  if (currentSlide == 4) {
+    currentSlide = 0;
+    show(currentSlide);
+    return currentSlide;
+  } else{
+    ++currentSlide;
+    show(currentSlide);
+  }
+}
+
+function prevSlide() {
+  if (currentSlide == 0) {
+    currentSlide = slides.length - 1;
+    show(currentSlide);
+    return currentSlide;
+  }else{
+    --currentSlide;
+    show(currentSlide);
+  }
+}
+
+show(currentSlide);
