e4111f594d761deabce44fbd4cf7dfd4bf55afb2
Anton Sokolov <zvuk45@bk.ru>
2019-12-09 12:24:00 +0500

    надо смерджить с мастером

--- js/main.js
+++ js/main.js
@@ -87,10 +87,13 @@ iconBurg.addEventListener("click", function(){
 var hamburger = document.getElementById('hamburger');
 var sectMenu = document.getElementById('hamburger-menu');
 var hamburgerOpen = document.getElementById('hamburger-open');
+var html = document.querySelector('html');
 
 hamburger.addEventListener("click", function(){
     sectMenu.style.top = 0;
+    html.style.overflowY = 'hidden';
 });
 hamburgerOpen.addEventListener("click", function(){
     sectMenu.style.top = 100 + 'rem';
+    html.style.overflowY = 'auto';
 });
