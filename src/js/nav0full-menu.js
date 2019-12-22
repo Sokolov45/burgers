b0c4f1e5ec5d4670883b5447ffcd98f82cdb72a5
Anton Sokolov <zuk45@bk.ru>
2019-12-22 19:44:52 +0500

    осталось немного..=)

---
+++ src/js/nav0full-menu.js
@@ -0,0 +1,10 @@
+var fullMenuLink = document.getElementsByClassName('full-menu-link');
+var hamburgerMenu = document.getElementById('hamburger-menu');
+
+for (let index = 0; index < fullMenuLink.length; index++) {
+
+    fullMenuLink[index].addEventListener('click', () => {
+        hamburgerMenu.style.top = 100 +'rem';
+    })
+    
+}
